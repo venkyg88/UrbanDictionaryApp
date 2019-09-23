4d5740f26a96b03376aa5488df280320c52824fe
Venkat Gonuguntala <venkyg88@gmail.com>
2019-09-23 00:41:02 -0400

    Add workmanager and constraints to it for performing background operations

--- app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -1,3 +1,33 @@
 
 
 package com.example.android.devbyteviewer.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.DictionariesRepository
+import retrofit2.HttpException
+
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters):
+        CoroutineWorker(appContext, params) {
+
+    companion object {
+        const val WORK_NAME = "RefreshDataWorker"
+    }
+
+    /**
+     * A coroutine-friendly method to do your work.
+     */
+    override suspend fun doWork(): Payload {
+        val database = getDatabase(applicationContext)
+        val repository = DictionariesRepository(database)
+        return try {
+            repository.refreshDictionareis()
+            Payload(Result.SUCCESS)
+        } catch (e: HttpException) {
+            Payload(Result.RETRY)
+        }
+    }
+}
